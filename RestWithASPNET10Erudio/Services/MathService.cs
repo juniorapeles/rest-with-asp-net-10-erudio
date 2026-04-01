8113dcd6f6d6616a49f37c6ced4d480d68a3d0f2
Eli Apeles <eli.apeles@integraerp.com.br>
2026-04-01 19:53:02 -0300

    Refactor MathController

---
+++ RestWithASPNET10Erudio/Services/MathService.cs
@@ -0,0 +1,31 @@
+﻿namespace RestWithASPNET10Erudio.Services
+{
+    public class MathService
+    {
+
+        private const double DIVISOR_MEDIA = 2d;
+
+        public decimal Sum(decimal firstNumber, decimal secondNumber) => firstNumber + secondNumber;
+
+        public decimal Mean(double firstNumber, double secondNumber)
+        {
+            return (decimal)((firstNumber + secondNumber) / DIVISOR_MEDIA);
+        }
+
+        public decimal Sub(decimal firstNumber, decimal secondNumber) => firstNumber - secondNumber;
+        public decimal Mul(decimal firstNumber, decimal secondNumber) => firstNumber * secondNumber;
+
+        public decimal Div(decimal firstNumber, decimal secondNumber)
+        {
+            if (secondNumber == 0) throw new DivideByZeroException("Division by zero is not Allowed.");
+            return firstNumber / secondNumber;
+        }
+
+        public double Sqrt(double number)
+        {
+            if (number < 0) throw new ArgumentOutOfRangeException(
+                "Cannot calculate the square root of negative number.");
+            return Math.Sqrt(number);
+        }
+    }
+}
---
+++ RestWithASPNET10Erudio/Utils/NumberHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace RestWithASPNET10Erudio.Utils
+{
+    public class NumberHelper
+    {
+
+        public static bool TryConvertToDouble(string strNumber, out double result)
+        {
+            return double.TryParse(
+                strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out result
+            );
+        }
+
+        public static bool TryConvertToDecimal(string strNumber, out decimal result)
+        {
+            return decimal.TryParse(
+                strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out result
+            );
+        }
+    }
+}
+
