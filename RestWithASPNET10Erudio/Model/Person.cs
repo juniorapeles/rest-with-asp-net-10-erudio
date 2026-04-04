5d1385bfe4223e07102799f08152431a6186752d
Eli Apeles <eli.apeles@integraerp.com.br>
2026-04-04 10:32:15 -0300

    mapeamento de person

--- RestWithASPNET10Erudio/Model/Person.cs
+++ RestWithASPNET10Erudio/Model/Person.cs
@@ -1,11 +1,34 @@
-﻿namespace RestWithASPNET10Erudio.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNET10Erudio.Model
 {
+    [Table("person")]
     public class Person
     {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
-        public string FirstName { get; set; }   
-        public string LastName { get; set; }   
-        public string Address { get; set; }   
+
+        [Required]
+        [MaxLength(80)]
+        [Column("first_name", TypeName ="varchar(80")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        [Column("last_name", TypeName = "varchar(80")]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Column("address", TypeName = "varchar(100")]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(6)]
+        [Column("gender", TypeName = "varchar(6")] // Male, Female, Other
         public string Gender { get; set; }   
     }
 }
