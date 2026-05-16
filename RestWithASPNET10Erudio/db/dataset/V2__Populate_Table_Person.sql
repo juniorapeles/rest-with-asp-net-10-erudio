0eefb9591e94479279a687618f330bd0a35de3b6
Eli Apeles <eli.apeles@integraerp.com.br>
2026-05-16 13:59:12 -0300

    Adiciona dataset de pessoas (V2) com dados em ingles

---
+++ RestWithASPNET10Erudio/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO dbo.person (first_name, last_name, address, gender)
+VALUES ('Ayrton', 'Senna', 'Pacaembu Avenue, 1000 - Sao Paulo, SP', 'Male');
+
+INSERT INTO dbo.person (first_name, last_name, address, gender)
+VALUES ('Leonardo', 'da Vinci', 'Via Roma, 1 - Florence, Italy', 'Male');
+
+INSERT INTO dbo.person (first_name, last_name, address, gender)
+VALUES ('Nelson', 'Mandela', 'Vilakazi Street, 123 - Johannesburg, South Africa', 'Male');
