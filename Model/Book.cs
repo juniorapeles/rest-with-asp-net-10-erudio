13625cb572db07fc1e8c32b4ad6a78a2122dd0af
Eli Apeles <eli.apeles@integraerp.com.br>
2026-05-16 18:38:31 -0300

    fix: correct Book entity mapping

--- Model/Book.cs
+++ Model/Book.cs
@@ -1,6 +1,30 @@
-﻿namespace RestWithASPNET10Erudio.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNET10Erudio.Model
 {
+    [Table("books")]
     public class Book
     {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required]
+        [Column("title", TypeName = "varchar(max)")]
+        public string Title { get; set; }
+
+        [Required]
+        [Column("author", TypeName = "varchar(max)")]
+        public string Author { get; set; }
+
+        [Required]
+        [Column("price", TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Column("launch_date", TypeName = "DATETIME2(6)")]
+        public DateTime LaunchDate { get; set; }
     }
 }
